1550ce4d0f7c2e5ec3651967f59ce7f7d58bfb3d
Davor Runje <davor@airt.ai>
2024-04-11 13:02:43 +0000

    wip

--- .devcontainer/python-3.10/setup.sh
+++ .devcontainer/python-3.10/setup.sh
@@ -1,5 +1,5 @@
 # needed to make sure default python is 3.10 instead of 3.11
-sudo ln -s -f /usr/local/bin/python3.10 /usr/bin/python3
+#sudo ln -s -f /usr/local/bin/python3.10 /usr/bin/python3
 
 # update pip
 pip install --upgrade pip

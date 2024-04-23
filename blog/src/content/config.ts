bd832947c09d451f3975abe83fc99409640c5de8
Kumaran Rajendhiran <kumaran@airt.ai>
2024-04-23 06:53:34 +0000

    Remove unnecessary files

--- blog/src/content/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection } from 'astro:content';
-import { i18nSchema } from '@astrojs/starlight/schema';
-import { docsAndBlogSchema } from 'starlight-blog/schema';
-
-export const collections = {
-  docs: defineCollection({ schema: docsAndBlogSchema }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-};

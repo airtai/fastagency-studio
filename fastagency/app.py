61793d208e01b54a5d3d4d5dc20a40b99f7555e9
Harish Mohan Raj <harish@airt.ai>
2024-04-26 18:50:36 +0530

    WIP

--- fastagency/app.py
+++ fastagency/app.py
@@ -28,42 +28,43 @@ async def validate_model(type: str, name: str, model: Dict[str, Any]) -> None:
 
 # new routes by Harish
 
-all_models: Dict[int, List[Optional[Dict[str, Any]]]] = {}
+all_models: Dict[int, Dict[str, List[Optional[Dict[str, Any]]]]] = {}
 
 
 def find_model(user_id: int, uuid: str) -> Dict[str, Any]:
     if user_id not in all_models:
         raise HTTPException(status_code=404, detail="User not found")
     for model in all_models[user_id]:
-        if model and model["uuid"] == uuid:
-            return model
+        if model and model["uuid"] == uuid:  # type: ignore
+            return model  # type: ignore
     raise HTTPException(status_code=404, detail="Model not found")
 
 
 class User(BaseModel):
     user_id: int
+    property_type: str
 
 
 @app.post("/user/models")
 def models(user: User) -> List[Optional[Dict[str, Any]]]:
-    return all_models.get(user.user_id, [])
+    user_models = all_models.get(user.user_id, {})
+    return user_models.get(user.property_type, [])
 
 
 class Model(BaseModel):
     uuid: str
+    api_key: str
+    property_type: str
+    property_name: str
     user_id: int
-    model: str
-    base_url: str
-    api_type: str
-    api_version: Optional[str] = None
 
 
 @app.post("/user/models/add")
-def models_add(model: Model) -> List[Optional[Dict[str, Any]]]:
-    models = all_models.setdefault(model.user_id, [])
+def models_add(model: Model) -> Dict[str, Any]:
+    user_models = all_models.setdefault(model.user_id, {})
     model_dict = model.model_dump()
-    models.append(model_dict)
-    return models
+    user_models.setdefault(model.property_type, []).append(model_dict)
+    return user_models
 
 
 class ModelUpdate(BaseModel):
@@ -80,8 +81,8 @@ def models_update(model_update: ModelUpdate) -> Dict[str, Any]:
     model = find_model(model_update.user_id, model_update.uuid)
     updated_model = model_update.model_dump()
     updated_model["uuid"] = model["uuid"]
-    all_models[model_update.user_id].remove(model)
-    all_models[model_update.user_id].append(updated_model)
+    all_models[model_update.user_id].remove(model)  # type: ignore
+    all_models[model_update.user_id].append(updated_model)  # type: ignore
     return updated_model
 
 
@@ -93,5 +94,5 @@ class ModelDelete(BaseModel):
 @app.delete("/user/models/delete")
 def models_delete(model_delete: ModelDelete) -> Dict[str, str]:
     model = find_model(model_delete.user_id, model_delete.uuid)
-    all_models[model_delete.user_id].remove(model)
+    all_models[model_delete.user_id].remove(model)  # type: ignore
     return {"detail": "Model deleted successfully"}

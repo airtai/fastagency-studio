7b8882b119fd9be19ffc999018d1dec643fd38dd
Harish Mohan Raj <harish@airt.ai>
2024-04-28 16:35:40 +0530

    Add Examples page

--- app/src/client/components/Button.tsx
+++ app/src/client/components/Button.tsx
@@ -3,12 +3,14 @@ import { MouseEventHandler, ReactNode } from 'react';
 interface ButtonProps {
   onClick: MouseEventHandler<HTMLButtonElement>;
   label?: string;
+  theme?: 'light' | 'dark';
 }
 
-export default function Button({ onClick, label }: ButtonProps) {
+export default function Button({ onClick, label, theme = 'dark' }: ButtonProps) {
+  const buttonBG = theme === 'dark' ? 'bg-airt-primary' : 'bg-airt-secondary';
   return (
     <button
-      className='rounded-md px-3.5 py-2.5 text-sm  bg-airt-primary text-airt-font-base   hover:bg-opacity-85 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+      className={`rounded-md px-3.5 py-2.5 text-sm  ${buttonBG} text-airt-font-base   hover:bg-opacity-85 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
       onClick={onClick}
     >
       {label}

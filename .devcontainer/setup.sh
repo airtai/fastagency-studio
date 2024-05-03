db314e618dc2969976d8c19f9cdd1bff4f33d200
Kumaran Rajendhiran <kumaran@airt.ai>
2024-05-03 12:25:15 +0000

    Add UserModel table and include python prisma ORM integration

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,3 +9,8 @@ pre-commit install
 
 # install wasp
 curl -sSL https://get.wasp-lang.dev/installer.sh | sh
+
+cd app && wasp build && wasp db migrate-dev && cd ..
+cat schema.prisma.template ./app/.wasp/build/db/schema.prisma > /tmp/schema.prisma && mv /tmp/schema.prisma schema.prisma
+prisma generate --schema=schema.prisma --generator=pyclient
+rm schema.prisma

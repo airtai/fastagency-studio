cf97c221631f7fa05172cd0a328274565faefa81
Harish Mohan Raj <harish@airt.ai>
2024-05-06 18:51:32 +0530

    Run uvicorn with multiple workers

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,4 +3,4 @@
 prisma migrate deploy
 prisma generate --schema=schema.prisma --generator=pyclient
 
-uvicorn fastagency.app:app --workers 1 --host 0.0.0.0 --proxy-headers
+uvicorn fastagency.app:app --workers 8 --host 0.0.0.0 --proxy-headers

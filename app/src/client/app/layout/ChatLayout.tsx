60263602d41358865c7471ad157a8d062cd347b1
Harish Mohan Raj <harish@airt.ai>
2024-05-08 10:27:24 +0530

    WIP

---
+++ app/src/client/app/layout/ChatLayout.tsx
@@ -0,0 +1,108 @@
+import { type Chat } from 'wasp/entities';
+
+import { useAuth } from 'wasp/client/auth';
+import { useState, ReactNode, FC, useRef, useEffect } from 'react';
+import Header from '../../admin/components/Header';
+import ChatSidebar from '../../components/ChatSidebar';
+import ChatForm from '../../components/ChatForm';
+import { useHistory } from 'react-router-dom';
+
+interface Props {
+  children?: ReactNode;
+  handleFormSubmit: any;
+  currentChatDetails?: Chat | null;
+  triggerChatFormSubmitMsg?: string | null;
+  refetchAllChatDetails: boolean;
+}
+
+const ChatLayout: FC<Props> = ({
+  children,
+  handleFormSubmit,
+  currentChatDetails,
+  triggerChatFormSubmitMsg,
+  refetchAllChatDetails,
+}) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { data: user } = useAuth();
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!user) {
+      history.push('/login');
+    } else {
+      if (!user.hasPaid && user.isSignUpComplete) {
+        history.push('/pricing');
+      }
+    }
+  }, [user, history]);
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollTo({
+          top: scrollRef.current.scrollHeight,
+          behavior: 'smooth',
+        });
+      }
+    });
+
+    if (scrollRef.current) {
+      observer.observe(scrollRef.current, { childList: true, subtree: true });
+    }
+
+    return () => observer.disconnect();
+  }, []);
+  // make call to api -> from action file access conversation entity and pass it to openai
+  // get response from openai and save it against the conversation
+
+  const wrapperClass = document.body.classList.contains('server-error')
+    ? 'h-[calc(100vh-85px)]'
+    : 'h-screen';
+
+  return (
+    <div className='dark:bg-boxdark-2 dark:text-bodydark bg-captn-light-blue'>
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className={`flex ${wrapperClass} overflow-hidden`}>
+        {/* <!-- ===== Sidebar Start ===== --> */}
+        <ChatSidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+          refetchAllChatDetails={refetchAllChatDetails}
+        />
+        {/* <!-- ===== Sidebar End ===== --> */}
+
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div className='relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden'>
+          {/* <!-- ===== Header Start ===== --> */}
+          <Header
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+            user={user}
+          />
+          {/* <!-- ===== Header End ===== --> */}
+
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main className='flex-auto overflow-y-auto' ref={scrollRef}>
+            <div>{children}</div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+          {currentChatDetails ? (
+            <ChatForm
+              handleFormSubmit={handleFormSubmit}
+              currentChatDetails={currentChatDetails}
+              triggerChatFormSubmitMsg={triggerChatFormSubmitMsg}
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </div>
+  );
+};
+
+export default ChatLayout;
---
+++ app/src/client/components/ChatForm.tsx
@@ -0,0 +1,89 @@
+import { type Chat } from 'wasp/entities';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+
+interface ChatFormProps {
+  handleFormSubmit: (userQuery: string) => void;
+  currentChatDetails: Chat;
+  triggerChatFormSubmitMsg?: string | null;
+}
+
+export default function ChatForm({
+  handleFormSubmit,
+  currentChatDetails,
+  triggerChatFormSubmitMsg,
+}: ChatFormProps) {
+  const [formInputValue, setFormInputValue] = useState('');
+
+  const formInputRef = useCallback(
+    async (node: any) => {
+      if (node !== null && triggerChatFormSubmitMsg) {
+        // @ts-ignore
+        await handleFormSubmit(triggerChatFormSubmitMsg, true);
+      }
+      // if (node !== null && userSelectedActionMessage) {
+      //   // @ts-ignore
+      //   await handleFormSubmit(userSelectedActionMessage, true);
+      // }
+    },
+    [triggerChatFormSubmitMsg]
+    // [triggerChatFormSubmitMsg, userSelectedActionMessage]
+  );
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!currentChatDetails.showLoader) {
+      const target = event.target as HTMLFormElement;
+      const userQuery = target.userQuery.value;
+      setFormInputValue('');
+      handleFormSubmit(userQuery);
+    }
+  };
+
+  return (
+    <div data-testid='chat-form' className='mt-2 mb-2'>
+      <form onSubmit={handleSubmit} className=''>
+        <label
+          htmlFor='search'
+          className='mb-2 text-sm font-medium text-captn-dark-blue sr-only dark:text-captn-light-cream'
+        >
+          Search
+        </label>
+        <div className='relative bottom-0 left-0 right-0 flex items-center justify-between m-1'>
+          <input
+            type='search'
+            id='userQuery'
+            name='search'
+            className='block rounded-lg w-full h-12 text-sm text-captn-light-cream bg-captn-dark-blue focus:outline-none focus:ring-0 focus:border-captn-light-blue'
+            placeholder='Message Capt’n...'
+            required
+            ref={formInputRef}
+            value={formInputValue}
+            onChange={(e) => setFormInputValue(e.target.value)}
+          />
+          <button
+            type='submit'
+            className={`text-captn-light-cream bg-captn-cta-green hover:bg-captn-cta-green-hover absolute right-2 font-medium rounded-lg text-sm px-1.5 py-1.5`}
+          >
+            <span className=''>
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                className='text-captn-light-cream'
+              >
+                <path
+                  d='M7 11L12 6L17 11M12 18V7'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}

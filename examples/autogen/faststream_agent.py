adf30273e316ee3800a1e26aa06389c979539a1c
Kumaran Rajendhiran <kumaran@airt.ai>
2024-05-13 15:48:23 +0000

    Add IOWebsockets class

--- examples/autogen/faststream_agent.py
+++ examples/autogen/faststream_agent.py
@@ -1,7 +1,8 @@
 import json
 import random
 from os import environ
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Callable
+from queue import Queue
 
 from asyncer import asyncify
 from faststream import FastStream, Logger
@@ -23,27 +24,68 @@ app = FastStream(broker)
 stream = JStream(name="request_response", subjects=["request.*", "response.*", "initiate.*"])
 
 
-async def request_handler(body: Dict[str, Any], msg: NatsMessage, logger: Logger) -> None:
-    raw_message = msg.raw_message
+class IOWebsockets():
+    def __init__(self, publisher: Callable[..., None], publish_subject: str) -> None:
+        self.queue = Queue()
+        self._publisher = publisher
+        self._publish_subject = publish_subject
 
-    subject = raw_message.subject
-    client_id = subject.split(".")[1]
-    reply_subject = raw_message.reply
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n", flush: bool = False) -> None:
+        """Print data to the output stream.
 
-    await msg.ack()
-    logger.info(
-        f"Received a message on '{subject=} {reply_subject=}': {body=} -> from {process_id=}"
-    )
+        Args:
+            objects (any): The data to print.
+            sep (str, optional): The separator between objects. Defaults to " ".
+            end (str, optional): The end of the output. Defaults to "\n".
+            flush (bool, optional): Whether to flush the output. Defaults to False.
+        """
+        xs = sep.join(map(str, objects)) + end
+        # self._websocket.send(xs)
+        self._publisher(xs, self._publish_subject)
 
-    if "msg" not in body:
-        init_msg = "What is the weather like today?"
-    else:
-        init_msg = body["msg"]
+    def input(self, prompt: str = "", *, password: bool = False) -> str:
+        """Read a line from the input stream.
 
-    chat_result = await asyncify(chat_with_weatherman)(message=init_msg)
+        Args:
+            prompt (str, optional): The prompt to display. Defaults to "".
+            password (bool, optional): Whether to read a password. Defaults to False.
 
-    for c in chat_result.chat_history:
-        await broker.publish(json.dumps(c), f"response.{client_id}")
+        Returns:
+            str: The line read from the input stream.
+
+        """
+        # if prompt != "":
+        #     self._websocket.send(prompt)
+
+        msg = self.queue.get()
+        self.queue.task_done()
+
+        return msg.decode("utf-8") if isinstance(msg, bytes) else msg
+
+    async def request_handler(self, body: Dict[str, Any], msg: NatsMessage, logger: Logger) -> None:
+        raw_message = msg.raw_message
+
+        subject = raw_message.subject
+        client_id = subject.split(".")[1]
+        reply_subject = raw_message.reply
+
+        await msg.ack()
+        logger.info(
+            f"Received a message on '{subject=} {reply_subject=}': {body=} -> from {process_id=}"
+        )
+
+        if "msg" not in body:
+            init_msg = "What is the weather like today?"
+        else:
+            init_msg = body["msg"]
+
+        # ToDo: Use the print function to send the message to NATS
+        # TODo: When a new message arrives put it in self.queue and use input function to read message in autogen code
+
+        chat_result = await asyncify(chat_with_weatherman)(message=init_msg)
+
+        for c in chat_result.chat_history:
+            await broker.publish(json.dumps(c), f"response.{client_id}")
 
 
 @broker.subscriber(
@@ -68,11 +110,13 @@ async def initiate_handler(
 
     logger.info(f"Creating a new subscriber for {client_id=}")
 
+    iostream = IOWebsockets(broker.publish, f"response.{client_id}")
+
     subscriber = broker.subscriber(
         f"request.{client_id}",
         stream=stream,
     )
-    subscriber(request_handler)
+    subscriber(iostream.request_handler)
 
     broker.setup_subscriber(subscriber)
     await subscriber.start()

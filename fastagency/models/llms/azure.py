0b3f6d13aff3ce5b34e4b6ced221996615691010
Kumaran Rajendhiran <kumaran@airt.ai>
2024-05-17 07:26:26 +0000

    Add tests for llms

--- fastagency/models/llms/azure.py
+++ fastagency/models/llms/azure.py
@@ -1,10 +1,12 @@
-from typing import Annotated, Any, Literal
+from typing import Annotated, Any, Dict, Literal
 from uuid import UUID
 
-from pydantic import Field, HttpUrl
+from asyncer import syncify
+from pydantic import AfterValidator, Field, HttpUrl
 from typing_extensions import TypeAlias
 
 from ...constants import AZURE_API_VERSIONS_LITERAL
+from ...db.helpers import find_model_using_raw
 from ..base import Model
 from ..registry import register
 
@@ -19,11 +21,18 @@ class AzureOAIAPIKey(Model):
     api_key: Annotated[str, Field(description="The API Key from Azure OpenAI")]
 
     @classmethod
-    def create_autogen(cls, model_id: UUID, user_id: UUID) -> Any:
-        pass
+    def create_autogen(cls, model_id: UUID, user_id: UUID) -> str:
+        my_model_dict = syncify(find_model_using_raw)(model_id, user_id)
+        my_model = cls(**my_model_dict["json_str"])
+
+        return my_model.api_key
 
 
 AzureOAIAPIKeyRef: TypeAlias = AzureOAIAPIKey.get_reference_model()  # type: ignore[valid-type]
+
+# Pydantic adds trailing slash automatically to URLs, so we need to remove it
+# https://github.com/pydantic/pydantic/issues/7186#issuecomment-1691594032
+URL = Annotated[HttpUrl, AfterValidator(lambda x: str(x).rstrip("/"))]
 
 
 @register("llm")
@@ -38,8 +47,8 @@ class AzureOAI(Model):
     api_key: AzureOAIAPIKeyRef
 
     base_url: Annotated[
-        HttpUrl, Field(description="The base URL of the Azure OpenAI API")
-    ] = HttpUrl(url="https://api.openai.com/v1")
+        URL, Field(description="The base URL of the Azure OpenAI API")
+    ] = URL(url="https://api.openai.com/v1")
 
     api_type: Annotated[
         Literal["azure"],
@@ -54,5 +63,27 @@ class AzureOAI(Model):
     ] = "latest"
 
     @classmethod
-    def create_autogen(cls, model_id: UUID, user_id: UUID) -> Any:
-        pass
+    def create_autogen(cls, model_id: UUID, user_id: UUID) -> Dict[str, Any]:
+        my_model_dict = syncify(find_model_using_raw)(model_id, user_id)
+        my_model = cls(**my_model_dict["json_str"])
+
+        api_key_dict = syncify(find_model_using_raw)(my_model.api_key.uuid, user_id)
+        api_key_model = my_model.api_key.get_data_model()(**api_key_dict["json_str"])
+        api_key = api_key_model.create_autogen(my_model.api_key.uuid, user_id)
+
+        config_list = [
+            {
+                "model": my_model.model,
+                "api_key": api_key,
+                "base_url": str(my_model.base_url),
+                "api_type": my_model.api_type,
+                "api_version": my_model.api_version,
+            }
+        ]
+
+        llm_config = {
+            "config_list": config_list,
+            "temperature": 0,
+        }
+
+        return llm_config

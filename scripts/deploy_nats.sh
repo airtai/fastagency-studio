712665f3edb14402a3fecda3cf68abb1ce6654e2
Kumaran Rajendhiran <kumaran@airt.ai>
2024-05-22 07:55:23 +0000

    Add variable to delete jetstream subjects

--- scripts/deploy_nats.sh
+++ scripts/deploy_nats.sh
@@ -24,6 +24,10 @@ ssh_command="ssh -o StrictHostKeyChecking=no -i key.pem azureuser@$DOMAIN"
 echo "INFO: stopping already running NATS container"
 $ssh_command "export PORT='$PORT' && docker compose -f nats-docker-compose.yaml down || echo 'No NATS container available to stop'"
 $ssh_command "docker container prune -f || echo 'No stopped containers to delete'"
+if [ "$CLEAR_JETSTREAM" = true ] ; then
+    echo "INFO: clearing JetStream data"
+    $ssh_command "sudo rm -rf ./jetstream"
+fi
 
 echo "INFO: SCPing nats-docker-compose.yaml and config files"
 scp -i key.pem ./nats-docker-compose.yaml azureuser@$DOMAIN:/home/azureuser/nats-docker-compose.yaml

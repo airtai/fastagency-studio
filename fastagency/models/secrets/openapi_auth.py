a54d64fafa5c51d7202157c7218ddc7c78aa6dc2
Kumaran Rajendhiran <kumaran@airt.ai>
2024-05-29 09:18:31 +0000

    Add OpenAPIAuth

---
+++ fastagency/models/secrets/openapi_auth.py
@@ -0,0 +1,32 @@
+from typing import Annotated, Tuple
+from uuid import UUID
+
+from asyncer import syncify
+from pydantic import Field
+
+from ...db.helpers import find_model_using_raw
+from ..base import Model
+from ..registry import register
+
+
+@register("secret")
+class OpenAPIAuth(Model):
+    username: Annotated[
+        str,
+        Field(
+            description="username for openapi routes authentication",
+        ),
+    ]
+    password: Annotated[
+        str,
+        Field(
+            description="password for openapi routes authentication",
+        ),
+    ]
+
+    @classmethod
+    def create_autogen(cls, model_id: UUID, user_id: UUID) -> Tuple[str, str]:
+        my_model_dict = syncify(find_model_using_raw)(model_id, user_id)
+        my_model = cls(**my_model_dict["json_str"])
+
+        return my_model.username, my_model.password

496777cde62c93af296cd1e0f37402464f3b589a
Kumaran Rajendhiran <kumaran@airt.ai>
2024-06-06 13:45:40 +0530

    Update azure openai api versions (#305)

--- fastagency/constants.py
+++ fastagency/constants.py
@@ -11,4 +11,13 @@ OPENAI_MODELS_LITERAL = Literal["gpt-4", "gpt-3.5-turbo"]
 
 ### Azure OpenAI
 
-AZURE_API_VERSIONS_LITERAL = Literal["2024-02-15-preview"]
+AZURE_API_VERSIONS_LITERAL = Literal[
+    "2023-05-15",
+    "2023-06-01-preview",
+    "2023-10-01-preview",
+    "2024-02-15-preview",
+    "2024-03-01-preview",
+    "2024-04-01-preview",
+    "2024-05-01-preview",
+    "2024-02-01",
+]

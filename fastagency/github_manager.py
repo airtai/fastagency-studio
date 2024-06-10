a4630dfcd89f9b7c6b27b35ff98bf5732f1941c5
Harish Mohan Raj <harish@airt.ai>
2024-06-11 00:43:27 +0530

    WIP: Add GH helper class

--- fastagency/github_manager.py
+++ fastagency/github_manager.py
@@ -1,9 +1,12 @@
+import argparse
 import logging
+import random
 import shutil
 import subprocess  # nosec B404
 import tempfile
 from os import environ
 from pathlib import Path
+from typing import Optional
 
 import requests
 
@@ -12,14 +15,17 @@ logging.basicConfig(level=logging.INFO)
 
 class GitHubManager:
     TEMPLATE_REPO_URL = "https://github.com/airtai/fastagency-wasp-app-template"
+    EXTRACTED_TEMPLATE_DIR_NAME = "fastagency-wasp-app-template-main"
+    ARTIFACTS_DIR = ".tmp_fastagency_setup_artifacts"
 
-    def __init__(self) -> None:
+    def __init__(
+        self, fly_api_token: str, fastagency_application_uuid: str, github_token: str
+    ) -> None:
         """GitHubManager class."""
         self.template_repo_url = GitHubManager.TEMPLATE_REPO_URL
-        self.fly_api_token = GitHubManager._get_env_var("FLY_API_TOKEN")
-        self.fastagency_application_uuid = GitHubManager._get_env_var(
-            "FASTAGENCY_APPLICATION_UUID"
-        )
+        self.fly_api_token = fly_api_token
+        self.fastagency_application_uuid = fastagency_application_uuid
+        self.github_token = github_token
 
     @staticmethod
     def _get_env_var(var_name: str) -> str:
@@ -27,42 +33,161 @@ class GitHubManager:
             raise OSError(f"{var_name} not set in the environment")
         return environ[var_name]
 
-    def _download_template_repo(self, temp_dir: str) -> None:
+    def _download_template_repo(self, temp_dir_path: Path) -> None:
         owner, repo = self.template_repo_url.rstrip("/").rsplit("/", 2)[-2:]
         zip_url = f"https://github.com/{owner}/{repo}/archive/refs/heads/main.zip"
         response = requests.get(zip_url, timeout=10)
         if response.status_code == 200:
-            temp_dir_path = Path(temp_dir)
             zip_path = temp_dir_path / f"{repo}.zip"
             with Path(zip_path).open("wb") as file:
                 file.write(response.content)
 
             shutil.unpack_archive(str(zip_path), str(temp_dir_path))
-
-            for item in (temp_dir_path / f"{repo}-main").iterdir():
-                shutil.move(str(item), str(temp_dir_path))
-            shutil.rmtree(temp_dir_path / f"{repo}-main")
             zip_path.unlink()
+
+            command = (
+                f"ls -la {temp_dir_path}/{GitHubManager.EXTRACTED_TEMPLATE_DIR_NAME}"
+            )
+            self._run_cli_command(command)
         else:
             raise Exception(f"Error downloading repository: {response.status_code}")
 
-    def run_cli_command(self, command: str) -> None:
+    def _run_cli_command(
+        self, command: str, cwd: Optional[str] = None, print_output: bool = False
+    ) -> None:
         try:
             logging.info(f"Running command: {command}")
-            result = subprocess.run(command, check=True, capture_output=True)  # nosec B603
-            logging.info(result.stdout.decode())
+            # nosemgrep: python.lang.security.audit.subprocess-shell-true.subprocess-shell-true
+            result = subprocess.run(
+                command,
+                check=True,
+                capture_output=True,
+                shell=True,
+                text=True,
+                cwd=cwd,  # nosec B602
+            )
+            if print_output:
+                logging.info(result.stdout)
+            logging.info("Command executed successfully")
         except subprocess.CalledProcessError as e:
-            logging.error(e.stderr.decode())
+            logging.error(f"Command '{command}' failed with error: {e.output}")
             raise
 
-    def execute(self) -> None:
+    def _login(self) -> None:
+        command = f"echo {self.github_token} | gh auth login --with-token"
+        self._run_cli_command(command)
+
+    def _logout(self) -> None:
+        command = "gh auth logout"
+        self._run_cli_command(command)
+
+    def _create_new_repository(self, temp_dir_path: Path, max_retries: int) -> None:
+        repo_name = "test-fastagency-template"
+        setup_artifacts_path = temp_dir_path / GitHubManager.ARTIFACTS_DIR
+        setup_artifacts_path.mkdir(parents=True, exist_ok=True)
+
+        for attempt in range(max_retries):
+            try:
+                command = f"gh repo create {repo_name} --public > {setup_artifacts_path}/create-repo.txt"
+                self._run_cli_command(command, cwd=str(temp_dir_path))
+                break
+            except Exception as e:
+                # check if error contains "Name already exists on this account" string
+                if attempt < max_retries - 1:
+                    # add random 5 digit number to the repo name
+                    repo_name = (
+                        f"test-fastagency-template-{random.randint(10000, 99999)}"  # nosec B311
+                    )
+                    logging.info(
+                        f"Repository name already exists. Retrying with a new name: {repo_name}"
+                    )
+                else:
+                    logging.error(e)
+                    raise
+
+    @staticmethod
+    def _get_account_name_and_repo_name(create_cmd_output_file_path: Path) -> str:
+        with create_cmd_output_file_path.open("r") as file:
+            url_parts = file.read().split("/")
+            account_and_repo_name = "/".join(url_parts[-2:])
+        return account_and_repo_name.strip()
+
+    def _initialize_git_and_push(self, temp_dir_path: Path) -> None:
+        cwd = str(temp_dir_path / GitHubManager.EXTRACTED_TEMPLATE_DIR_NAME)
+
+        # initialize a git repository
+        command = "git init"
+        self._run_cli_command(command, cwd=cwd)
+
+        # add all files to the git repository
+        command = "git add ."
+        self._run_cli_command(command, cwd=cwd)
+
+        # commit the changes
+        command = 'git commit -m "Create a new FastAgency SaaS application"'
+        self._run_cli_command(command, cwd=cwd)
+
+        # git remote add origin
+        command = "git branch -M main"
+        self._run_cli_command(command, cwd=cwd)
+
+        # get the account name and repo name
+        create_cmd_output_file_path = Path(
+            f"{temp_dir_path}/{GitHubManager.ARTIFACTS_DIR}/create-repo.txt"
+        )
+        account_and_repo_name = self._get_account_name_and_repo_name(
+            create_cmd_output_file_path
+        )
+
+        # set the remote origin
+        command = f"git remote add origin git@github.com:{account_and_repo_name}.git"
+        self._run_cli_command(command, cwd=cwd)
+
+        # Set GitHub Actions secrets
+        self._set_github_actions_secrets(cwd)
+
+        # push the changes
+        command = "git push -u origin main"
+        self._run_cli_command(command, cwd=cwd)
+
+    def _set_github_actions_secrets(self, cwd: str) -> None:
+        secrets = {
+            "FLY_API_TOKEN": self.fly_api_token,
+            "FASTAGENCY_APPLICATION_UUID": self.fastagency_application_uuid,
+        }
+
+        for key, value in secrets.items():
+            command = f'gh secret set {key} --body "{value}" --app actions'
+            self._run_cli_command(command, cwd=cwd, print_output=True)
+
+    def execute(self, max_retries: int = 5) -> None:
         with tempfile.TemporaryDirectory() as temp_dir:
+            temp_dir_path = Path(temp_dir)
+
             # Download the public repository
-            self._download_template_repo(temp_dir)
+            self._download_template_repo(temp_dir_path)
+
+            # authenticate to GitHub
+            self._login()
+
+            # Create a new repository
+            self._create_new_repository(temp_dir_path, max_retries)
+
+            # Initialize the git repository
+            self._initialize_git_and_push(temp_dir_path)
+
+            # logout from GitHub
+            self._logout()
 
 
 def main() -> None:
-    manager = GitHubManager()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fly_token", help="Fly.io token")
+    parser.add_argument("uuid", help="Application UUID")
+    parser.add_argument("gh_token", help="GitHub token")
+    args = parser.parse_args()
+
+    manager = GitHubManager(args.fly_token, args.uuid, args.gh_token)
     manager.execute()
 
 
--- tests/test_github_manager.py
+++ tests/test_github_manager.py
@@ -1,3 +1,4 @@
+import subprocess
 import tempfile
 import zipfile
 from pathlib import Path
@@ -9,18 +10,29 @@ from fastagency.github_manager import GitHubManager
 
 
 @pytest.fixture()
-def github_manager(monkeypatch: pytest.MonkeyPatch) -> GitHubManager:
-    with monkeypatch.context() as mp:
-        # Set environment variables
-        mp.setenv("FLY_API_TOKEN", "some-token")
-        mp.setenv("FASTAGENCY_APPLICATION_UUID", "some-uuid")
+def github_manager() -> GitHubManager:
+    fly_api_token = "some-token"
+    fastagency_application_uuid = "some-uuid"
+    github_token = "some-github-token"
 
-        return GitHubManager()
+    return GitHubManager(fly_api_token, fastagency_application_uuid, github_token)
 
 
-def test_github_manager_without_env_vars() -> None:
-    with pytest.raises(OSError, match="FLY_API_TOKEN not set in the environment"):
-        GitHubManager()
+def test_get_account_name_and_repo_name() -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_path = Path(temp_dir)
+        artifacts_dir = temp_dir_path / GitHubManager.ARTIFACTS_DIR
+        artifacts_dir.mkdir()
+
+        create_cmd_output_file_path = artifacts_dir / "create-repo.txt"
+        with create_cmd_output_file_path.open("w") as f:
+            f.write("https://github.com/account-name/repo-name")
+
+        expected = "account-name/repo-name"
+        actual = GitHubManager._get_account_name_and_repo_name(
+            create_cmd_output_file_path
+        )
+        assert actual == expected
 
 
 @patch("requests.get")
@@ -48,7 +60,7 @@ def test_download_template_repo(
             zip_file.writestr(f"{repo_name}-main", "dummy content")
 
         with patch.object(Path, "open", mock_open()) as mocked_file:
-            github_manager._download_template_repo(temp_dir)
+            github_manager._download_template_repo(Path(temp_dir))
 
             # Ensure the zip file is written
             # mocked_file.assert_called_once_with(zip_path, 'wb')
@@ -58,5 +70,180 @@ def test_download_template_repo(
         mock_unpack_archive.assert_called_once_with(str(zip_path), str(temp_dir_path))
 
         # Ensure the directory structure is correct after moving files
-        assert (temp_dir_path / "dummy_file.txt").exists()
+        assert (temp_dir_path / repo_main_dir / "dummy_file.txt").exists()
         assert len(list(temp_dir_path.iterdir())) == 1
+
+
+@patch("subprocess.run")
+def test_run_cli_command(mock_run: MagicMock, github_manager: GitHubManager) -> None:
+    command = "gh auth login"
+    github_manager._run_cli_command(command)
+    mock_run.assert_called_once_with(
+        command, check=True, capture_output=True, shell=True, text=True, cwd=None
+    )
+
+
+@patch("subprocess.run")
+def test_login(mock_run: MagicMock, github_manager: GitHubManager) -> None:
+    github_manager._login()
+    expected_command = "echo some-github-token | gh auth login --with-token"
+    mock_run.assert_called_once_with(
+        expected_command,
+        check=True,
+        capture_output=True,
+        shell=True,
+        text=True,
+        cwd=None,
+    )
+
+
+@patch("subprocess.run")
+def test_create_new_repository(
+    mock_run: MagicMock, github_manager: GitHubManager
+) -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_path = Path(temp_dir)
+        temp_dir_path.mkdir(parents=True, exist_ok=True)
+        github_manager._create_new_repository(temp_dir_path, max_retries=1)
+        expected_command = f"gh repo create test-fastagency-template --public > {temp_dir_path}/{GitHubManager.ARTIFACTS_DIR}/create-repo.txt"
+        mock_run.assert_called_once_with(
+            expected_command,
+            check=True,
+            capture_output=True,
+            shell=True,
+            text=True,
+            cwd=str(temp_dir_path),
+        )
+
+
+@patch("subprocess.run")
+def test_create_new_repository_retry(
+    mock_run: MagicMock, github_manager: GitHubManager
+) -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_path = Path(temp_dir)
+        temp_dir_path.mkdir(parents=True, exist_ok=True)
+
+        # Simulate "Name already exists on this account" error for the first two attempts
+        mock_run.side_effect = [
+            subprocess.CalledProcessError(
+                1, "gh", "Name already exists on this account"
+            )
+        ] * 2 + [None]
+
+        # Call the method
+        github_manager._create_new_repository(temp_dir_path, max_retries=3)
+
+        # Check that the method was called three times
+        assert mock_run.call_count == 3
+
+
+@patch("subprocess.run")
+def test_create_new_repository_retry_fail(
+    mock_run: MagicMock, github_manager: GitHubManager
+) -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_path = Path(temp_dir)
+        temp_dir_path.mkdir(parents=True, exist_ok=True)
+
+        # Simulate "Name already exists on this account" error for all attempts
+        mock_run.side_effect = subprocess.CalledProcessError(
+            1, "gh", "Name already exists on this account"
+        )
+
+        # Call the method and expect an exception
+        with pytest.raises(
+            Exception, match="Command 'gh' returned non-zero exit status 1."
+        ) as e:
+            github_manager._create_new_repository(temp_dir_path, max_retries=3)
+
+        assert "Name already exists on this account" in str(e)
+
+        # Check that the method was called three times
+        assert mock_run.call_count == 3
+
+
+@patch("subprocess.run")
+def test_set_github_actions_secrets(
+    mock_run: MagicMock, github_manager: GitHubManager
+) -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        github_manager._set_github_actions_secrets(cwd=temp_dir)
+        expected_commands = [
+            'gh secret set FLY_API_TOKEN --body "some-token" --app actions',
+            'gh secret set FASTAGENCY_APPLICATION_UUID --body "some-uuid" --app actions',
+        ]
+        for command in expected_commands:
+            mock_run.assert_any_call(
+                command,
+                check=True,
+                capture_output=True,
+                shell=True,
+                text=True,
+                cwd=temp_dir,
+            )
+
+
+@patch("subprocess.run")
+def test_initialize_git_and_push(
+    mock_run: MagicMock, github_manager: GitHubManager
+) -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_path = Path(temp_dir)
+        extracted_template_dir = (
+            temp_dir_path / GitHubManager.EXTRACTED_TEMPLATE_DIR_NAME
+        )
+        extracted_template_dir.mkdir(parents=True, exist_ok=True)
+
+        with patch.object(
+            GitHubManager,
+            "_get_account_name_and_repo_name",
+            return_value="account/repo",
+        ):
+            github_manager._initialize_git_and_push(temp_dir_path)
+
+            expected_commands = [
+                "git init",
+                "git add .",
+                'git commit -m "Create a new FastAgency SaaS application"',
+                "git branch -M main",
+                "git remote add origin git@github.com:account/repo.git",
+                "git push -u origin main",
+            ]
+
+            for command in expected_commands:
+                mock_run.assert_any_call(
+                    command,
+                    check=True,
+                    capture_output=True,
+                    shell=True,
+                    text=True,
+                    cwd=str(extracted_template_dir),
+                )
+
+
+@patch("fastagency.github_manager.GitHubManager._initialize_git_and_push")
+@patch("fastagency.github_manager.GitHubManager._download_template_repo")
+@patch("fastagency.github_manager.GitHubManager._login")
+@patch("fastagency.github_manager.GitHubManager._logout")
+@patch("fastagency.github_manager.GitHubManager._create_new_repository")
+@patch("tempfile.TemporaryDirectory", new_callable=MagicMock)
+def test_execute(
+    mock_tempdir: MagicMock,
+    mock_create_repo: MagicMock,
+    mock_login: MagicMock,
+    mock_logout: MagicMock,
+    mock_download: MagicMock,
+    mock_init_git: MagicMock,
+    github_manager: GitHubManager,
+) -> None:
+    temp_dir_path = Path("/mock/temp/dir")
+    mock_tempdir.return_value.__enter__.return_value = temp_dir_path
+
+    github_manager.execute(max_retries=1)
+
+    mock_download.assert_called_once_with(temp_dir_path)
+    mock_login.assert_called_once()
+    mock_logout.assert_called_once()
+    mock_create_repo.assert_called_once_with(temp_dir_path, 1)
+    mock_init_git.assert_called_once_with(temp_dir_path)

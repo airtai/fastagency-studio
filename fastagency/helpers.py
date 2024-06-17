8111a75aada7828a5ea3edab689f03f7551fb54a
Davor Runje <davor@airt.ai>
2024-06-17 07:31:53 +0200

    Descriptions of functions and parameters added to Toolbox (#330)
    * refactoring
    
    * type fixing

---
+++ fastagency/helpers.py
@@ -0,0 +1,54 @@
+import uuid
+from typing import Any, Optional, Type, TypeVar, Union
+from uuid import UUID
+
+from fastapi import BackgroundTasks
+
+from fastagency.app import add_model
+
+from .db.helpers import find_model_using_raw
+from .models.base import Model, ObjectReference
+from .models.registry import Registry
+
+T = TypeVar("T", bound=Model)
+
+
+async def get_model_by_uuid(model_uuid: Union[str, UUID]) -> Model:
+    model_dict = await find_model_using_raw(model_uuid=model_uuid)
+
+    registry = Registry.get_default()
+    model = registry.validate(
+        type=model_dict["type_name"],
+        name=model_dict["model_name"],
+        model=model_dict["json_str"],
+    )
+
+    return model
+
+
+async def get_model_by_ref(model_ref: ObjectReference) -> Model:
+    return await get_model_by_uuid(model_ref.uuid)
+
+
+async def create_model_ref(
+    cls: Type[T],
+    type_name: str,
+    user_uuid: Union[str, UUID],
+    background_tasks: Optional[BackgroundTasks] = None,
+    **kwargs: Any,
+) -> ObjectReference:
+    model = cls(**kwargs)
+    model_uuid = uuid.uuid4()
+
+    await add_model(
+        user_uuid=str(user_uuid),
+        type_name=type_name,
+        model_name=cls.__name__,  # type: ignore [attr-defined]
+        model_uuid=str(model_uuid),
+        model=model.model_dump(),
+        background_tasks=background_tasks,  # type: ignore[arg-type]
+    )
+
+    model_ref = cls.get_reference_model()(uuid=model_uuid)
+
+    return model_ref
--- fastagency/models/teams/multi_agent_team.py
+++ fastagency/models/teams/multi_agent_team.py
@@ -5,7 +5,7 @@ from autogen import ConversableAgent, GroupChat, GroupChatManager
 from pydantic import Field
 
 from ..registry import Registry
-from ..toolboxes.toolbox import FunctionInfo
+from ..toolboxes.toolbox import Client
 from .base import TeamBaseModel, agent_type_refs, register_toolbox_functions
 
 __all__ = ["MultiAgentTeam"]
@@ -16,18 +16,18 @@ registry = Registry.get_default()
 class AutogenMultiAgentTeam:
     def __init__(
         self,
-        agents_and_functions: List[Tuple[ConversableAgent, List[FunctionInfo]]],
+        agents_and_clients: List[Tuple[ConversableAgent, List[Client]]],
     ) -> None:
-        self.agents = [agent for agent, _ in agents_and_functions]
-        self.functions = [functions for _, functions in agents_and_functions]
+        self.agents = [agent for agent, _ in agents_and_clients]
+        self.clients = [clients for _, clients in agents_and_clients]
 
-        for i, (agent, functions) in enumerate(agents_and_functions):
+        for i, (agent, clients) in enumerate(agents_and_clients):
             other_agents = [
                 other_agent
-                for j, (other_agent, _) in enumerate(agents_and_functions)
+                for j, (other_agent, _) in enumerate(agents_and_clients)
                 if i != j
             ]
-            register_toolbox_functions(agent, other_agents, functions)
+            register_toolbox_functions(agent, other_agents, clients)
 
     def initiate_chat(self, message: str) -> List[Dict[str, Any]]:
         groupchat = GroupChat(agents=self.agents, messages=[])
@@ -79,7 +79,7 @@ class MultiAgentTeam(TeamBaseModel):
     async def create_autogen(cls, model_id: UUID, user_id: UUID) -> Any:
         my_model = await cls.from_db(model_id)
 
-        agents_and_functions: List[Tuple[ConversableAgent, List[FunctionInfo]]] = []
+        agents_and_clients: List[Tuple[ConversableAgent, List[Client]]] = []
         for i in range(5):
             agent_property = getattr(my_model, f"agent_{i+1}")
             if agent_property is None:
@@ -89,9 +89,9 @@ class MultiAgentTeam(TeamBaseModel):
                 agent_property.uuid
             )
 
-            agent, functions = await agent_model.create_autogen(
+            agent, clients = await agent_model.create_autogen(
                 getattr(my_model, f"agent_{i+1}").uuid, user_id
             )
-            agents_and_functions.append((agent, functions))
+            agents_and_clients.append((agent, clients))
 
-        return AutogenMultiAgentTeam(agents_and_functions)
+        return AutogenMultiAgentTeam(agents_and_clients)

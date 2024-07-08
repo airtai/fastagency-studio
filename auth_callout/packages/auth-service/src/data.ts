9c519af362ffd22575a097f4d5cfb0b51d72fe42
Kumaran Rajendhiran <kumaran@airt.ai>
2024-07-08 09:14:18 +0000

    Remove unnecessary stuff

--- auth_callout/packages/auth-service/src/data.ts
+++ auth_callout/packages/auth-service/src/data.ts
@@ -1,28 +1,5 @@
 import { AuthToken, PrismaClient } from "@prisma/client";
-import fs from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
 
-export type User = {
-  readonly pass: string;
-  readonly account: string;
-};
-
-export type Room = {
-  readonly users: ReadonlyArray<string>;
-};
-
-export type Data = {
-  readonly users: Record<string, User>;
-  readonly rooms: Record<string, Room>;
-};
-
-export async function readData(): Promise<Data> {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const rawData = await fs.readFile(path.join(__dirname, "../../../data.json"), "utf8");
-  const data: Data = JSON.parse(rawData);
-  return data;
-}
 
 export type Auth = {
   user: string;

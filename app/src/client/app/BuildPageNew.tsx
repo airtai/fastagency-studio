40c06b223232081dd444bae8295325292c08d020
Harish Mohan Raj <harish@airt.ai>
2024-08-07 09:44:11 +0530

    WIP: Use TanStack Form

--- app/src/client/app/BuildPageNew.tsx
+++ app/src/client/app/BuildPageNew.tsx
@@ -4,12 +4,13 @@ import { type User } from 'wasp/entities';
 import { getModels, useQuery } from 'wasp/client/operations';
 import _ from 'lodash';
 import Select, { StylesConfig, SingleValue } from 'react-select';
-import type { FieldApi } from '@tanstack/react-form';
+import type { FieldApi, FormApi } from '@tanstack/react-form';
 import { useForm } from '@tanstack/react-form';
 
+import { validateForm } from 'wasp/client/operations';
 import { cn } from '../../shared/utils';
 import { useBuildPageNew } from '../hooks/useBuildPageNew';
-import { PropertiesSchema, ListOfSchemas } from '../interfaces/BuildPageInterfacesNew';
+import { PropertiesSchema, ListOfSchemas, Schema } from '../interfaces/BuildPageInterfacesNew';
 
 import CustomAuthRequiredLayout from './layout/CustomAuthRequiredLayout';
 import CustomSidebar, { navLinkItems } from '../components/CustomSidebar';
@@ -27,6 +28,7 @@ import { SelectInput } from '../components/form/SelectInput';
 import { PropertySchemaParser } from '../components/buildPage/PropertySchemaParser';
 import { TextInput } from '../components/form/TextInput';
 import { SECRETS_TO_MASK } from '../utils/constants';
+import { fi } from '@faker-js/faker';
 
 interface BuildPageProps {
   user: User;
@@ -288,7 +290,6 @@ export const ModelSelector = ({
   propertyName: string | undefined;
   setAddOrUpdateModel: React.Dispatch<React.SetStateAction<string>>;
 }) => {
-  console.log('propertySchemasList', propertySchemasList);
   const selectOptions = propertySchemasList.schemas.map((schema) => {
     return {
       value: schema.name,
@@ -328,6 +329,49 @@ export const ModelSelector = ({
   );
 };
 
+/* Dynamic form starts*/
+const getDefaultValues = (schema: Schema | {}) => {
+  const defaultValues: { [key: string]: any } = {};
+
+  if ('json_schema' in schema) {
+    Object.entries(schema.json_schema.properties).forEach(([key, property]: [string, any]) => {
+      defaultValues[key] = property.default || '';
+    });
+  }
+  return defaultValues;
+};
+
+type ValidationError = {
+  type: string;
+  loc: string[];
+  msg: string;
+  input: any;
+  url: string;
+  ctx?: any;
+};
+
+type ErrorOutput = { [key: string]: string };
+
+export function parseValidationErrors(errors: ValidationError[]): ErrorOutput {
+  const result: ErrorOutput = {};
+  errors.forEach((error) => {
+    const key = error.loc[error.loc.length - 1]; // Using the last item in 'loc' array as the key
+    result[key] = error.msg;
+  });
+  return result;
+}
+
+function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+  return (
+    <>
+      {field.state.meta.isTouched && field.state.meta.errors.length ? (
+        <em>{field.state.meta.errors.join(',')}</em>
+      ) : null}
+      {field.state.meta.isValidating ? 'Validating...' : null}
+    </>
+  );
+}
+
 export const DynamicForm = ({
   propertySchemasList,
   addOrUpdateModel,
@@ -335,54 +379,85 @@ export const DynamicForm = ({
   propertySchemasList: ListOfSchemas;
   addOrUpdateModel: string;
 }) => {
+  const [defaultValues, setDefaultValues] = useState<{ [key: string]: any }>({});
   // initiate the PropertySchemaParser with the propertySchemasList
   const propertySchemaParser = new PropertySchemaParser(propertySchemasList);
 
   // get the schema for the selected model
   const schema = propertySchemaParser.getSchemaForModel(addOrUpdateModel);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-
-  const generateHTML = (schema: any) => {
-    return Object.entries(schema.json_schema.properties).map(([key, property]: [string, any]) => {
-      if (key === 'uuid') {
-        return null;
-      }
-      return (
-        <div key={key} className='w-full mt-2'>
-          <form.Field
-            //@ts-ignore
-            name={`${key}`}
-            children={(field) => (
-              <>
-                <label htmlFor={key}>{property.title}</label>
-                <TextInput
-                  id={key}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e)}
-                  type={_.includes(SECRETS_TO_MASK, key) && typeof property.type === 'string' ? 'password' : 'text'}
-                  placeholder={property.description || ''}
-                />
-              </>
-            )}
-          />
-        </div>
-      );
-    });
-  };
+  console.log('defaultValues', defaultValues);
 
   const form = useForm({
-    defaultValues: {
-      name: '',
-      api_key: '',
-    },
-    onSubmit: async ({ value }) => {
-      // Do something with form data
-      console.log(value);
+    defaultValues: defaultValues,
+    onSubmit: async ({ value, formApi }) => {
+      try {
+        const validationURL: string = `models/${propertySchemasList.name}/${addOrUpdateModel}/validate`;
+        const isSecretUpdate = false;
+        const data = value;
+        const response = await validateForm({ data, validationURL, isSecretUpdate });
+        console.log('No errors. can submit the form');
+        // form can be added to the database here
+      } catch (error: any) {
+        try {
+          const errorMsgObj = JSON.parse(error.message);
+          const errors = parseValidationErrors(errorMsgObj);
+          Object.entries(errors).forEach(([key, value]) => {
+            formApi.setFieldMeta(key, (meta) => ({
+              ...meta,
+              errorMap: { onChange: value },
+            }));
+          });
+        } catch (e: any) {
+          // set form level error. something went wrong. Please try again later. Unable to submit the form
+          console.log(e);
+        }
+      }
     },
   });
+
+  useEffect(() => {
+    form.reset();
+    setDefaultValues(getDefaultValues(schema));
+  }, [schema]);
+
+  const generateHTML = (schema: Schema | {}) => {
+    if ('json_schema' in schema) {
+      return Object.entries(schema.json_schema.properties).map(([key, property]: [string, any]) => {
+        if (key === 'uuid') {
+          return null;
+        }
+        return (
+          <div key={key} className='w-full mt-2'>
+            <form.Field
+              //@ts-ignore
+              name={`${key}`}
+              children={(field) => (
+                <>
+                  <label htmlFor={key}>{property.title}</label>
+                  <TextInput
+                    id={key}
+                    // @ts-ignore
+                    value={field.state.value}
+                    // @ts-ignore
+                    onChange={(e) => field.handleChange(e)}
+                    type={_.includes(SECRETS_TO_MASK, key) && typeof property.type === 'string' ? 'password' : 'text'}
+                    placeholder={property.description || ''}
+                  />
+                  <FieldInfo field={field} />
+                </>
+              )}
+              validators={{
+                onChange: ({ value }) => undefined,
+              }}
+            />
+          </div>
+        );
+      });
+    } else {
+      return null;
+    }
+  };
 
   return (
     <form
@@ -400,7 +475,7 @@ export const DynamicForm = ({
             <button type='submit' disabled={!canSubmit}>
               {isSubmitting ? '...' : 'Submit'}
             </button>
-            <button type='reset' onClick={() => form.reset()}>
+            <button className='ml-2' type='reset' onClick={() => form.reset()}>
               Reset
             </button>
           </>

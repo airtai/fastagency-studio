fe12aa75629731ef605f1d076cb4609619b3ea3f
Harish Mohan Raj <harish@airt.ai>
2024-08-20 10:49:10 +0530

    WIP: Add tests

--- app/src/client/tests/UserProperty.test.tsx
+++ app/src/client/tests/UserProperty.test.tsx
@@ -8,10 +8,33 @@ import { renderInContext } from 'wasp/client/test';
 import { useQuery } from 'wasp/client/operations';
 import { mockProps } from './mocks';
 
+const mockRefetch = vi.fn();
+const userPropertiesMockResponse = [
+  {
+    uuid: 'test-4dd6-81af-4155de6a87ec',
+    user_uuid: 'test-user-uuid-563ec0df-3ecd-4d2a',
+    type_name: 'secret',
+    model_name: 'AzureOAIAPIKey',
+    json_str: {
+      name: '111',
+      api_key: '111111',
+    },
+    created_at: '2024-08-19T11:28:32.875000Z',
+    updated_at: '2024-08-19T11:28:32.875000Z',
+  },
+];
+
+const validateAndSaveMockResponse = {
+  json: {
+    name: 'My LLM',
+    api_key: 'test-api-key', // pragma: allowlist secret
+    uuid: 'test-uuid-563ec0df-3ecd-4d2a-a43a',
+  },
+};
+
 // Mock the wasp/client/operations
 vi.mock('wasp/client/operations', () => ({
   useQuery: vi.fn(() => ({
-    // todo: fix the data strucure. pass the user added models
     data: [
       { uuid: '1', type_name: 'testProperty', model_name: 'Model1', json_str: {} },
       { uuid: '2', type_name: 'testProperty', model_name: 'Model2', json_str: {} },
@@ -20,6 +43,8 @@ vi.mock('wasp/client/operations', () => ({
     refetch: vi.fn(),
   })),
   getModels: vi.fn(),
+  validateForm: vi.fn(() => Promise.resolve(validateAndSaveMockResponse)),
+  addUserModels: vi.fn(() => Promise.resolve(validateAndSaveMockResponse)),
 }));
 
 describe('UserProperty', () => {
@@ -144,7 +169,20 @@ describe('UserProperty', () => {
 });
 
 describe('End-to-End mock test for form save and resume functaionality', () => {
-  it('Should presist the user filled form data when pressed cancel on the resume flow', async () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    // Initial mock setup
+    mockRefetch.mockResolvedValue({ data: userPropertiesMockResponse });
+
+    (useQuery as Mock).mockReturnValue({
+      data: userPropertiesMockResponse,
+      isLoading: false,
+      refetch: mockRefetch,
+    });
+  });
+
+  it('Should persist user-filled form data when pressing cancel in the resume flow', async () => {
     const user = userEvent.setup();
     const agentProps = {
       ...mockProps,
@@ -194,6 +232,103 @@ describe('End-to-End mock test for form save and resume functaionality', () => {
 
       // check if the previous form is able to presist the user input
       expect(nameInput).toHaveValue('Test Name');
+    });
+  });
+  it('Should persist user-filled form data when a dependent property is created and the previous form is redisplayed', async () => {
+    const user = userEvent.setup();
+    const agentProps = {
+      ...mockProps,
+      activeProperty: 'agent',
+    };
+
+    const { getAllByRole, container } = renderInContext(<UserProperty {...agentProps} />);
+
+    // Click 'Add Agent' button
+    await user.click(screen.getByText('Add Agent'));
+
+    // Wait for the form to appear
+    await waitFor(() => {
+      expect(screen.getByText('AssistantAgent')).toBeInTheDocument();
+      expect(screen.getByText('LLM')).toBeInTheDocument();
+    });
+
+    // Update the agent Name
+    const agentNameInput = screen.getByLabelText('Name');
+    await user.type(agentNameInput, 'My Agent');
+
+    // Open the LLM dropdown
+    const selectElements = getAllByRole('combobox');
+    await user.click(selectElements[1]);
+
+    // Check the listbox content
+    const listbox = screen.getByRole('listbox');
+    expect(listbox.children.length).toBe(1);
+    expect(screen.getByText('Add new "LLM"')).toBeInTheDocument();
+
+    // Click the 'Add new "LLM"' option
+    await user.click(screen.getByText('Add new "LLM"'));
+
+    // Wait for the new LLM form to appear
+    await waitFor(() => {
+      expect(screen.getByText('Select LLM')).toBeInTheDocument();
+    });
+
+    // Update the LLM Name
+    const llmNameInput = screen.getByLabelText('Name');
+    await user.type(llmNameInput, 'My LLM');
+
+    // Mock the response for after a new LLM is created
+    const updatedUserProperties = [
+      ...userPropertiesMockResponse,
+      {
+        uuid: 'test-uuid-563ec0df-3ecd-4d2a-a43a',
+        user_uuid: 'test-user-uuid-563ec0df-3ecd-4d2a',
+        type_name: 'llm',
+        model_name: 'AzureOAI',
+        json_str: {
+          name: 'My LLM',
+          model: 'gpt-3.5-turbo',
+          api_key: {
+            name: 'AzureOAIAPIKey',
+            type: 'secret',
+            uuid: 'test-4dd6-81af-4155de6a87ec',
+          },
+          api_type: 'azure',
+          base_url: 'https://api.openai.com/v1',
+          api_version: '2024-02-01',
+          temperature: 0.8,
+        },
+        created_at: '2024-08-19T11:30:40.194000Z',
+        updated_at: '2024-08-19T11:30:40.194000Z',
+      },
+    ];
+
+    // Update the mock refetch function to return the new data after saving
+    mockRefetch.mockResolvedValueOnce({ data: updatedUserProperties });
+
+    // press save button
+    const saveButton = screen.getByRole('button', { name: 'Save' });
+    await user.click(saveButton);
+
+    // Wait for the refetch to be called
+    await waitFor(() => {
+      expect(mockRefetch).toHaveBeenCalled();
+    });
+
+    await waitFor(async () => {
+      expect(screen.getByText('AssistantAgent')).toBeInTheDocument();
+      expect(screen.getByText('LLM')).toBeInTheDocument();
+
+      // check if the previous form is able to presist the user input
+      expect(agentNameInput).toHaveValue('My Agent');
+      expect(screen.getByText('My LLM')).toBeInTheDocument();
+
+      // Open the LLM dropdown
+      const selectElements = getAllByRole('combobox');
+      await user.click(selectElements[1]);
+      // Check the listbox content
+      const listbox = screen.getByRole('listbox');
+      expect(listbox.children.length).toBe(2);
     });
   });
 });

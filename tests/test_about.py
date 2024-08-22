cf59183ef5c0844ce6f431e053dedcfd7e8a623d
Kumaran Rajendhiran <kumaran@airt.ai>
2024-08-22 11:42:54 +0000

    Update linting

--- tests/test_about.py
+++ tests/test_about.py
@@ -15,26 +15,26 @@ def test_help() -> None:
     assert actual == expected
 
 
-@pytest.mark.anthropic()
+@pytest.mark.anthropic
 def test_anthropic() -> None:
     assert 1 == 1
 
 
-@pytest.mark.azure_oai()
+@pytest.mark.azure_oai
 def test_azure_aoi() -> None:
     assert 1 == 1
 
 
-@pytest.mark.openai()
+@pytest.mark.openai
 def test_openai() -> None:
     assert 1 == 1
 
 
-@pytest.mark.togetherai()
+@pytest.mark.togetherai
 def test_togetherai() -> None:
     assert 1 == 1
 
 
-@pytest.mark.llm()
+@pytest.mark.llm
 def test_llm() -> None:
     assert 1 == 1
